342c328e89ae551f27e12a10753de386d43763d2
lucas <lucasvelasquez2001@gmail.com>
2023-06-17 03:18:08 -0300

    Avanzando la primera parte

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿
+using EspacioTarea;
+internal class Program
+{
+    const int cantTareas = 3;
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string[] descripciones = {"Jugar","Correr","Dormir"};
+        int[] duraciones = new int[3]; //declaramos y creamos el obj array que tiene espacio para 3 valores
+        duraciones[0] = 5;
+        duraciones[1] = 10;
+        duraciones[2] = 20;
+
+        var pendientes = new List<Tarea>();
+        var realizadas = new List<Tarea>();
+ 
+        for (int i = 0; i < cantTareas; i++)
+        {
+            var TareaX = new Tarea (i+1,descripciones[i],duraciones[i]);
+            pendientes.Add(TareaX);
+            //Console.WriteLine(TareaX.Descripcion);
+        }
+        //muestro tareas
+        for (int i = 0; i < cantTareas; i++)
+        {
+            Console.WriteLine(i+ "-Descripcion: " + pendientes[i].Descripcion + " Duracion: "+pendientes[i].Duracion);
+           
+        }
+
+
+        //cambiando tarea a realizada
+
+        string idIngresado = ""; //hacer con el tryparse practica parcial?
+        int idIngresadoInt = 0;
+
+        Console.WriteLine("Ingrese el numero de la tarea que decea mover a tareas realizadas: ");
+        idIngresado = Console.ReadLine(); 
+        int.TryParse(idIngresado, out idIngresadoInt);
+        Tarea TareaAux = pendientes[idIngresadoInt];
+        realizadas.Add(TareaAux);
+        pendientes.RemoveAt(idIngresadoInt);
+        
+
+
+        //para mostrar ocurre el problema de que cada lista tiene distinto tamaño y salta error
+        /*for (int i = 0; i < cantTareas; i++)
+        {
+            Console.WriteLine(i+ "-Descripcion: " + pendientes[i].Descripcion);   
+        }*/
+
+        //agregar la sumatoria de horas
+
+        
+
+
+    }
+}
---
+++ Tarea.cs
@@ -0,0 +1,46 @@
+namespace EspacioTarea
+{
+    public class Tarea
+    {
+        private int id;
+        private string descripcion;
+        private int duracion;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Descripcion
+        {
+            get => descripcion;
+            set => descripcion = value;
+        }
+        
+        public int Duracion
+        {
+            get => duracion;
+            set => duracion = value;
+        }
+        
+        public Tarea()
+        {
+
+        }
+        public Tarea(int id, string desc, int duracion)
+        {
+            Id = id;
+            Duracion = duracion;
+            Descripcion = desc;
+        }
+
+        //esto es en caso de querer concatenar dos atributos, pero como tengo que mostrar solo uno, lo devuelvo por la prop
+        /*public string MostrarTarea()
+        {
+            return "Descripcion: " + Descripcion;
+        }*/
+
+    }
+
+}
